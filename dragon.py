2e5ffbc6aca14630f3163a30821417d5e58a64d1
Jz069513 <124902245+Jz069513@users.noreply.github.com>
2023-02-09 08:32:20 -0700

    Update dragon.py

--- dragon.py
+++ dragon.py
@@ -92,3 +92,84 @@ while p<3:
   t.left(120)
   t.forward(50)
   p=p+1
+
+  
+  #withcolor:
+  
+  import turtle
+t = turtle.Turtle()
+s = turtle.Screen()
+d = 0
+p = 0
+
+t.color("blue")
+t.begin_fill()
+t.forward(220)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(220)
+t.right(90)
+t.forward(100)
+t.end_fill()
+t.right(180)
+t.forward(100)
+t.left(90)
+
+
+while d<4:
+  t.color("teal")
+  t.begin_fill()
+  t.forward(50)
+  t.right(90)
+  t.forward(50)
+  t.right(90)
+  t.forward(30)
+  t.right(90)
+  t.forward(50)
+  t.right(90)
+  t.forward(30)
+  t.end_fill()
+  d=d+1
+
+
+t.forward(20)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(40)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(90)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(90)
+
+t.begin_fill()
+t.left(180)
+t.forward(260)
+t.left(90)
+t.forward(30)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.end_fill()
+
+while p<4:
+  t.left(30)
+  t.forward(60)
+  t.right(120)
+  t.forward(40)
+  t.right(120)
+  t.forward(40)
+  p=p+1
+
+s.exitonclick()
