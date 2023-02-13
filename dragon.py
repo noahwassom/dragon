a15878d54b943b78b78e55b516e8067d0624904d
noahwassom <124376428+noahwassom@users.noreply.github.com>
2023-02-13 07:48:24 -0700

    Update dragon.py

--- dragon.py
+++ dragon.py
@@ -1,124 +1,3 @@
-import turtle
-t = turtle.Turtle()
-s = turtle.Screen()
-d = 0
-p = 0
-
-t.forward(220)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(220)
-t.right(90)
-t.forward(100)
-t.right(180)
-t.forward(100)
-t.left(90)
-
-while d<4:
-
-  t.forward(50)
-  t.right(90)
-  t.forward(50)
-  t.right(90)
-  t.forward(30)
-  t.right(90)
-  t.forward(50)
-  t.right(90)
-  t.forward(30)
-  d=d+1
-
-t.forward(20)
-t.left(90)
-t.forward(100)
-t.right(90)
-t.forward(40)
-t.left(90)
-t.forward(80)
-t.left(90)
-t.forward(90)
-t.left(90)
-t.forward(80)
-t.left(90)
-t.forward(90)
-
-t.left(180)
-t.forward(260)
-t.left(90)
-t.forward(30)
-t.right(90)
-t.forward(100)
-t.left(90)
-t.forward(20)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(20)
-t.left(90)
-t.forward(70)
-
-
-t.left(0)
-t.forward(30)
-t.right(120)
-t.forward(30)
-t.right(120)
-t.forward(30)
-t.left(120)
-t.forward(30)
-t.right(120)
-t.forward(30)
-t.left(120)
-t.forward(30)
-t.right(120)
-t.forward(30)
-t.right(120)
-t.forward(90)
-t.right(180)
-t.forward(100)
-t.left(90)
-t.forward(30)
-t.right(90)
-
-
-
-while p<3:
-  t.left(-0)
-  t.forward(50)
-  t.right(-120)
-  t.forward(50)
-  t.right(-120)
-  t.forward(50)
-  t.left(120)
-  t.forward(50)
-  p=p+1
-
-  t.forward(20)
-t.left(90)
-t.forward(80)
-t.right(90)
-t.forward(60)
-t.left(-0)
-t.forward(30)
-t.right(-120)
-t.forward(30)
-t.right(-120)
-t.forward(30)
-t.left(-120)
-t.forward(30)
-t.right(-120)
-t.forward(30)
-t.left(-120)
-t.forward(30)
-t.right(-120)
-t.forward(30)
-t.right(-120)
-t.forward(90)
-t.right(180)
-t.forward(100)
-  
-  ------------------------------------------------------------------------------------------------------
-  
   #withcolor:
   
 import turtle
@@ -191,6 +70,8 @@ t.forward(50)
 t.right(90)
 t.end_fill()
 
+
+
 while p<3:
   t.color("gray")
   t.begin_fill()
@@ -204,10 +85,6 @@ while p<3:
   t.forward(50)
   t.end_fill()
   p=p+1
-
-s.exitonclick()
-
---------------------------------------------------------------------------------------------------------------------------
 
 t.forward(30)
 t.left(90)
@@ -277,3 +154,6 @@ t.right(90)
 t.forward(20)
 t.right(90)
 t.forward(20)
+
+
+s.exitonclick()
